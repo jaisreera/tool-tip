ddb3b6f8fd1a6da9c3a523b7036d24dc78388794
rahul kumar shaw <8479946428ll@gmail.com>
2023-08-08 08:09:49 +0530

    add new path

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Tooltip from "./components/tooltip";
+import "./styles.css";
 
-function App() {
+const TooltipTarget = styled.span``;
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tooltip text="I am a tooltip" position="left" background="00adb5">
+        <TooltipTarget>Left</TooltipTarget>
+      </Tooltip>
+      <Tooltip text="I am a tooltip" position="top" background="222831">
+        <TooltipTarget>Top</TooltipTarget>
+      </Tooltip>
+      <Tooltip text="I am a tooltip" position="bottom" background="393e46">
+        <TooltipTarget>Bottom</TooltipTarget>
+      </Tooltip>
+      <Tooltip text="I am a tooltip" position="right" background="eeeeee">
+        <TooltipTarget>Right</TooltipTarget>
+      </Tooltip>
+      {/* <p>
+        This is a{" "}
+        <Tooltip text="I am within paragraph" position="top" styleMe={false}>
+          <TooltipTarget>tooltip</TooltipTarget>
+        </Tooltip>{" "}
+        within a paragraph.
+      </p> */}
     </div>
   );
 }
-
-export default App;
